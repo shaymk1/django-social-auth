f1de8b2f75f951dd288023e566a67bffc36fe9bc
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-07-29 15:26:18 +0200

    first commit with django installed, project and app created

--- README.md
+++ README.md
@@ -1,14 +1,65 @@
-# django-social-auth | django-allauth | Django OAuth2 |Django SSO|Sign in with Google Django
-Creating a social authentication / sign up app with django 
 
-# Functional requirements:
-⋅⋅* If user does not have a social media stated, they can sign in manually
-⋅⋅* Sign in with the social media above
-⋅⋅* Success messages after both manual anf social sign in
-⋅⋅* After sign in, an account is automatically created for you
-⋅⋅* The social account the user logged in  with should be displayed in settings
-⋅⋅* Ability to add other social accounts
-⋅⋅* Ability to remove any social media accounts logged in with
-⋅⋅* Ability for the user to sign out
+# django-social-auth
+
+This is a project to showcase how you can use Social Sign On with Django.
+
+## Features
+
+- Manual sign on using form
+- Ability to Sign in with the social media
+- After sign in, an account is automatically created for you
+- The social account the user logged in with should be displayed in settings
+- Sign Out
+
+## Installation
+
+- In your terminal, first clone the repository
+- CD into the main project folder
+- Open the repository in VS Code using:
+
+```python
+
+code .
+
+```
+
+- Create virtual enviroment:
+
+```python
 
 
+ py -m venv '#virtual environment name'
+
+```
+
+- Activate the virtual enviroment(windos):
+
+```python
+
+# nameofvirtualenviroment/scripts/activate
+
+```
+
+- Install dependencies using:
+
+```python
+
+pip install -r requirements.txt
+
+```
+
+- Run your server using:
+
+```python
+
+py manage.py runserver
+
+```
+
+## Dependencies
+
+### Here are the Dependencies used
+  
+- Django
+- HTML
+- CSS
--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,4 @@
 
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -7,10 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m0-i#a95!kj8*@@nfr9eyy3q7t&c&qz9o^^eh--fkytcj%8+i-'
+SECRET_KEY = 'django-insecure-t##khd**f72gyhkvdnz*r=sh9vu4s5x9*5d55%ho-ma&45v*8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -27,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -61,22 +61,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -103,17 +99,15 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
